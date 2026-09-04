eb2d685e4ac32d553481ab45c5d4328c5112ef1f
Parvez Saifi <parvezs2442@gmail.com>
2026-09-04 15:06:07 +0530

    feat: add auth service routing through gateway

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,9 +1,25 @@
 import express from "express"
+import dotenv from "dotenv"
+import proxy from "express-http-proxy"
+import cors from "cors"
+
+
+dotenv.config();
 
 const app = express();
-
 app.use(express.json())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials:true,
+}))
 
+
+app.use("/auth", proxy(process.env.AUTH_URL))
+
+
+app.get("/", (req,res) => {
+    res.send("Hello from Gateway")
+})
 
 
 app.listen(3000, () => {
---
+++ backend/services/auth/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors(
+    {
+        origin:process.env.FRONTEND_URL,
+        credentials:true,
+    }
+))
+
+const PORT = process.env.PORT || 3001
+
+
+app.get("/", (req,res) => {
+    res.send("Hello from Auth")
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening at PORT ${PORT}`)
+})
+
